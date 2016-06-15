63ccdcaa4542b12f2bb523b1babb60f340ae53c7
Matthias Schmidt <matthias.schmidt@gmail.com>
2016-06-15 22:25:01 +0200

    Using bitstring syntax to denote 18 bytes connect package

--- testclient/squeezeclient.erl
+++ testclient/squeezeclient.erl
@@ -9,10 +9,14 @@ client() ->
     % This stream of bytes represents a valid client connect 
     % from a softsqueeze player starting with HELO
 
-    Payload = << 16#48, 16#45, 16#4c, 16#4f, 16#00, 16#00, 16#00, 
-                 16#0a, 16#06, 16#02, 16#4a, 16#bb, 16#d2, 16#68, 
-                 16#a9, 16#cf, 16#80, 16#00 >>,
+    % Payload = << 16#48, 16#45, 16#4c, 16#4f, 16#00, 16#00, 16#00, 
+    %             16#0a, 16#06, 16#02, 16#4a, 16#bb, 16#d2, 16#68, 
+    %             16#a9, 16#cf, 16#80, 16#00 >>,
 
+    % We can use the hex-stream from wireshark if we give the
+    % Size in bits (here 144)
+    Payload = << 16#48454c4f0000000a06024abbd268a9cf8000:144 >>,
+    
      % to make it runnable on one machine
     {ok, Sock} = gen_tcp:connect(Destination, ?SQEEZE_PORT, 
                                  [binary, {packet, 0}]),
