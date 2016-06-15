c27064f810800ecc2e54815aed380fab1f87f4fa
Matthias Schmidt <matthias.schmidt@gmail.com>
2016-06-15 23:39:16 +0200

    Adding new startup proggy and bin-to-hex printer for debugging.

---
+++ squeezeling.erl
@@ -0,0 +1,25 @@
+-module(squeezeling).
+
+-export([start/0, loop/1]).
+
+-define ( SQUEEZE_PORT, 3483 ).
+
+% Run Softsqueze like this:
+% java -Dslimserver=localhost -jar SoftSqueeze.jar
+
+% Run this proggy as:
+% c("squeezeling"), squeezeling:start().
+
+start() ->
+    io:format("Squeezeling v0.1 running on port ~w~n", [?SQUEEZE_PORT]),
+	socket_server:start(?MODULE, ?SQUEEZE_PORT, {?MODULE, loop}).
+loop(Socket) ->
+    case gen_tcp:recv(Socket, 0) of
+        {ok, Data} ->
+            io:format("Got data~n"),
+            io:format(bin_to_hex:bin_to_hex(Data)),
+            gen_tcp:send(Socket, "Hi stranger\n"),
+            loop(Socket);
+        {error, closed} ->
+            ok
+    end.
