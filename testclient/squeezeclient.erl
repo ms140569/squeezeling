6d561dfa27ea6b1cc4439bcdb535d41b203fac83
Matthias Schmidt <Matthias.Schmidt@diva-e.com>
2016-06-15 17:49:54 +0200

    Adding first valid bitstream client connection

--- testclient/squeezeclient.erl
+++ testclient/squeezeclient.erl
@@ -1,13 +1,36 @@
 -module(squeezeclient).
--export ([client/0]).
+-export ([client/0, getDeviceId/1]).
 
 -define ( SQEEZE_PORT, 3483 ).
 
 client() ->
     Destination = "localhost",
+
+    % This stream of bytes represents a valid client connect 
+    % from a softsqueeze player starting with HELO
+
+    Payload = << 16#48, 16#45, 16#4c, 16#4f, 16#00, 16#00, 16#00, 
+                 16#0a, 16#06, 16#02, 16#4a, 16#bb, 16#d2, 16#68, 
+                 16#a9, 16#cf, 16#80, 16#00 >>,
+
      % to make it runnable on one machine
     {ok, Sock} = gen_tcp:connect(Destination, ?SQEEZE_PORT, 
                                  [binary, {packet, 0}]),
-    ok = gen_tcp:send(Sock, "HELO"),
+    ok = gen_tcp:send(Sock, Payload),
     ok = gen_tcp:close(Sock).
+
+
+getDeviceId(Id) ->
+    DeviceIds = #{
+      squeezebox => 2, 
+      softsqueeze => 3,
+      squeezebox2 => 4,
+      transporter => 5,
+      softsqueeze3 => 6,
+      receiver => 7,
+      squeezeslave => 8,
+      controller => 9
+      },
+    
+    maps:get(Id, DeviceIds).
 
