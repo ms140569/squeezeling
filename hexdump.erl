5dd64d0c7335e5021455b397a36f0d430b5ad86d
Matthias Schmidt <Matthias.Schmidt@diva-e.com>
2016-06-20 10:20:38 +0200

    Now with proper colons in between.

--- hexdump.erl
+++ hexdump.erl
@@ -11,7 +11,7 @@ bin_to_hexstr(Bin) ->
     lists:flatten([io_lib:format("~2.16.0B:", [X]) || 
                       X <- binary_to_list(Bin)]).
 
-% V2
+% V2 - pattern matching auf signaturen
 
 hex2(Val) when Val < 16 ->
     "0" ++ hex_(Val);
@@ -22,7 +22,7 @@ hex2(Val) ->
 hex_(Val) ->
     integer_to_list(Val, 16).
 
-% V3
+% V3 - pattern matching per case in der funktion
 % Test 0, 1, 15, 16, 128, 255, 256
 
 hex(Val) ->
@@ -40,5 +40,13 @@ test()->
 dump(Bin) when byte_size(Bin) == 0 -> "";
 dump(Bin) -> dump_(binary_to_list(Bin), []).
 
-dump_(InputList, OutputList) -> InputList.
+dump_([], Acc) ->
+    Acc;
+dump_([Head|Tail], Acc) ->
+    case length(Acc) > 0 of
+        true -> dump_(Tail, Acc ++ ":" ++ hex(Head));
+        false -> dump_(Tail, Acc ++ hex(Head))
+    end.
+
+
 
