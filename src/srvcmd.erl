c8eaff46a6da3d90ab61fd970e2a4363acccabd1
Matthias Schmidt <Matthias.Schmidt@diva-e.com>
2016-06-27 16:16:38 +0200

    Adding server-command generating module

---
+++ src/srvcmd.erl
@@ -0,0 +1,21 @@
+-module(srvcmd).
+% -export ([client/0, getDeviceId/1]).
+-compile(export_all).
+
+-define ( SQEEZE_PORT, 3483 ).
+-define ( DESTINATION, "localhost" ).
+
+cmd(Cmd, Payload) ->
+    % size - 2 bytes, command header eg. "strm" - 4 bytes, payload
+    Size = size(Payload) + 4,
+    StrCmd = list_to_binary(command(Cmd)),
+
+    << Size:1/big-integer-unit:16, 
+       StrCmd/binary, 
+       Payload/binary >>.
+
+command(Cmd) ->
+    case Cmd of
+        strm -> "strm";           
+        _  -> undef
+    end.
