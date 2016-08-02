91ed13359019be79a7a4bfab601c9cdb2ca732b9
Matthias Schmidt <matthias.schmidt@gmail.com>
2016-08-02 22:30:23 +0200

    Adding first version which turns lines into assignment pairs. Rough.

--- src/ir/ir.erl
+++ src/ir/ir.erl
@@ -1,7 +1,32 @@
 -module(ir).
 -compile(export_all).
 
+stripComment(Line) ->
+    case string:str(Line, "#") > 0 of
+        true -> string:substr(Line, 1, string:str(Line, "#") - 1);
+        false -> Line
+    end.
+
+fullstrip(Line) ->
+    re:replace(Line, "\\s+", "", [global,{return,list}]).
+
+lineToList(Line) ->
+    assignmentToList(fullstrip(stripComment(Line))).
+
+assignmentToList(Assign) ->
+    string:tokens(Assign, "=").
+
 parse(Filename) ->
+    parseLines(readLines(Filename)).
+
+parseLines(Lines) ->
+    Lx = lists:map(fun(L) -> lineToList(L) end, Lines),
+    % This filters out the empty Lists in the List caused by empty Lines
+    % this is a hack, which should be fixed ASAP. 
+    % Empty lines should not be placed in the outer list in the first place
+    lists:filter(fun(X) -> length(X) /= 0 end, Lx).
+
+readLines(Filename) ->
     {ok, File} = file:open(Filename, read),
     line_reader(File).
 
